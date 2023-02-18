a73cad0a6bc18aa01ce7a28a31b63300fc18a16e
Matheus Amarante <matheusamarante1@gmail.com.br>
2023-02-18 00:50:16 -0300

    enviano para o git preciso fazer um readme ainda

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+
+const apiroutes = require('./src/routes');
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    
+    useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error) => {
+    console.log("erro: ", error.message);
+});
+
+
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(fileupload());
+
+server.use(express.static(__dirname+'/public'));
+
+server.use('/', apiroutes);
+
+server.listen(process.env.PORT, () =>{
+    console.log( `Rodando no endereço: ${process.env.BASE}` );
+});
