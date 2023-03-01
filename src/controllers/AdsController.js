2e58c6f46269c485dbeca65149ac24a37080cf7c
Matheus Amarante <matheusamarante1@gmail.com.br>
2023-03-01 00:47:51 -0300

    indo para as partes finais

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -4,6 +4,7 @@ const jimp = require('jimp');
 const Category = require('../models/Category');
 const User = require('../models/User');
 const Ad = require('../models/Ad');  
+const StateModel = require('../models/State');
 
 
 const addImage = async (buffer) => {
@@ -89,8 +90,35 @@ module.exports = {
     },
     getList: async (req, res) => {
         let { sort = 'asc', offset = 0, limit = 8, q, cat, state  } = req.query;
-        
-        const adsData = await Ad.find({status: true}).exec();
+        let filters = {status: true};
+        let total = 0;
+
+        if(q) { //filtros
+            filters.title = {'$regex': q, '$options': 'i'};
+        }
+
+        if(cat) {
+            const c = await Category.findOne({slug: cat}).exec();
+            if(c) {
+                filters.category = c._id.toString();
+            }
+        }
+
+        if (state) {
+            const s = await StateModel.findOne({name: state.toUpperCase()}).exec();
+            if (s) {
+                filters.state = s._id.toString();
+            }
+        }
+
+        const adsTotal = await Ad.find(filters).exec();
+        total = adsTotal.length;
+
+        const adsData = await Ad.find(filters)
+            .sort({dateCreated: (sort=='desc'?-1:1)})
+            .skip(parseInt(offset))
+            .limit(parseInt(limit))
+            .exec();
         let ads = [];
         for(let i in adsData) {
             let image;
